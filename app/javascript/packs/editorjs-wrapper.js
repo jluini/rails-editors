dcbd75423327a1454cd4f281ef54d10745fd8552
jluini <jluini@gmail.com>
2020-08-21 18:59:35 -0300

    Add header plugin and i18n (in spanish)

--- app/javascript/packs/editorjs-wrapper.js
+++ app/javascript/packs/editorjs-wrapper.js
@@ -1,4 +1,5 @@
 import EditorJS from '@editorjs/editorjs';
+import Header from '@editorjs/header';
 
 class EditorJSWrapper {
   constructor(holderId, options) {
@@ -6,9 +7,70 @@ class EditorJSWrapper {
     this.editor = new EditorJS({
       holder: holderId,
       autofocus: true,
+      
+      /**
+       * Tools list
+       */
       tools: {
-        // add editorjs tools here
+        header: Header,
       },
+      
+      /**
+       * I18n configuration.
+       */
+      i18n: {
+        messages: {
+          ui: {
+            "blockTunes": {
+              "toggler": {
+                "Click to tune": "Clic para editar bloque",
+                "or drag to move": "o arrastra para mover"
+              }
+            },
+            "inlineToolbar": {
+              "converter": {
+                "Convert to": "Transformar en"
+              }
+            },
+            "toolbar": {
+              "toolbox": {
+                "Add": "Agregar"
+              }
+            }
+          },
+          toolNames: {
+            "Text": "Texto normal",
+            "Link": "Vínculo",
+            "Bold": "Negrita",
+            "Italic": "Cursiva",
+            "Heading": "Título",
+          },
+          tools: {
+            "link": {
+              "Add a link": "Destino del vínculo..."
+            },
+            "stub": {
+              "The block can not be displayed correctly.": "El bloque no puede mostrarse (no se reconoce su tipo)"
+            },
+            "header": {},
+          },
+          blockTunes: {
+            "delete": {
+              "Delete": "Eliminar bloque"
+            },
+            "moveUp": {
+              "Move up": "Subir bloque"
+            },
+            "moveDown": {
+              "Move down": "Bajar bloque"
+            }
+          }
+        }
+      },
+      
+      /**
+       * Custom options you may pass.
+       */
       ...options
     });
   }
