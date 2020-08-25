526618c468b7ee43a8a727395910176c54166bbf
jluini <jluini@gmail.com>
2020-08-25 16:55:57 -0300

    Add TinyMCE editor test

---
+++ app/controllers/tiny_mce_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+#
+# TinyMce controller.
+#
+class TinyMceController < ApplicationController
+  def test; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   
+  # EditorJS
   get '/editorjs-test', to: 'editorjs#test', as: 'editorjs-test'
   get '/editorjs-showroom', to: 'editorjs#showroom', as: 'editorjs-showroom'
-  
   post '/uploadFile', to: 'editorjs#upload_file'
   post '/fetchUrl', to: 'editorjs#fetch_url'
+  
+  # TinyMCE
+  get '/tiny-mce/test', to: 'tiny_mce#test', as: 'tinymce_test'
 end
