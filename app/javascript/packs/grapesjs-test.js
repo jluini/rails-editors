4d8f8342aa0b468d5d1ef9c45c7160083cbdba24
jluini <jluini@gmail.com>
2020-08-29 18:35:48 -0300

    Instantiate basic GrapesJS editor

--- app/javascript/packs/grapesjs-test.js
+++ app/javascript/packs/grapesjs-test.js
@@ -1,5 +1,17 @@
 import grapesjs from 'grapesjs';
+import 'grapesjs/dist/css/grapes.min.css';
 
-console.log('Hello from grapejs-test');
-
-console.log(grapesjs);
+const editor = grapesjs.init({
+  // Indicate where to init the editor. You can also pass an HTMLElement
+  container: '#gjs',
+  // Get the content for the canvas directly from the element
+  // As an alternative we could use: `components: '<h1>Hello World Component!</h1>'`,
+  fromElement: true,
+  // Size of the editor
+  height: '300px',
+  width: 'auto',
+  // Disable the storage manager for the moment
+  storageManager: false,
+  // Avoid any default panel
+  panels: { defaults: [] },
+});
